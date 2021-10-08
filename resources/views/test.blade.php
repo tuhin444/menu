ab125b51b1bceac81766cebc54eef25eb9ec534f
tuhin444 <tuhincse444@gmail.com>
2021-10-08 22:33:46 +0600

    first commit

---
+++ resources/views/test.blade.php
@@ -0,0 +1,85 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous"/>
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="{{asset('css/style.css')}}">
+
+    <title>Hello, world!</title>
+  </head>
+  <body>
+  <div class="container">
+  	<div class="row">
+  		 <nav>
+  		 	<div class="logo">
+  		 		Testing Menu
+  		 	</div>
+  		 	<label for="btn" class="icon">
+  		 		
+  		 		<span class="fa fa-bars"></span>
+  		 		<input type="checkbox" id="btn">
+  		 	</label>
+  		 	<ul>
+  		 		<li><a href="#">Home</a></li>
+  		 		<li><a href="#">Home</a></li>
+  		 		<li><a href="#">Home</a></li>
+  		 		<li>
+  		 			<label for="btn-1" class="show">More +</label>
+
+  		 			<a href="#">Sub  menu</a>
+  		 			<input type="checkbox" id="btn-1">
+  		 			<i class="fas fa-plus-square"></i>
+  		 			<ul>
+  		 				<li><a href="#">Home</a></li>
+  		 				<li><a href="#">Home</a></li>
+  		 				<li><a href="#">Home</a></li>
+  		 				<li><a href="#">Home</a></li>
+  		 				<li><a href="#">Home</a>
+  		 					<li>
+  		 						<a href="#">sub menu</a>
+  		 						<ul>
+					 				<li><a href="#">Home</a></li>
+					 				<li><a href="#">Home</a></li>
+					 				<li><a href="#">Home</a></li>
+  		 						</ul>
+  		 				</li>
+  		 			</ul>
+  		 		</li>
+  		 		<li>
+  		 			<label for="btn-2" class="show">More +</label>
+
+  		 			<a href="#">Sub  menu</a>
+  		 			<input type="checkbox" id="btn-2">
+  		 			<i class="fas fa-plus-square"></i>
+  		 			<ul>
+  		 				<li><a href="#">Home</a></li>
+  		 				<li><a href="#">Home</a></li>
+  		 				<li><a href="#">Home</a></li>
+  		 				<li><a href="#">Home</a></li>
+  		 				<li><a href="#">Home</a>
+  		 					<li>
+  		 						<a href="#">sub menu</a>
+  		 						<ul>
+					 				<li><a href="#">Home</a></li>
+					 				<li><a href="#">Home</a></li>
+					 				<li><a href="#">Home</a></li>
+  		 						</ul>
+  		 				</li>
+  		 			</ul>
+  		 		</li>
+  		 	</ul>
+  		 	
+  		 </nav>
+  	</div>
+  </div>
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+  </body>
+</html>
